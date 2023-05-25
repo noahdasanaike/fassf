989c5dda7591335febd935626ef219afa75ca0a7
Noah Dasanaike <noahdasanaike@college.harvard.edu>
2023-05-25 16:55:26 -0400

    Update faster_st_filter.R

--- base functions/faster_st_filter.R
+++ base functions/faster_st_filter.R
@@ -3,8 +3,12 @@ faster_st_filter <- function(original, intersecting, quiet = FALSE){
   if(st_crs(original) != st_crs(intersecting) & st_crs(original) != "EPSG:3857"){
     if(quiet == FALSE){print("correcting CRS")}
     
-    original <- original %>% st_as_sf() %>% st_transform(crs = "EPSG:3857")
-    intersecting <- intersecting %>% st_as_sf() %>% st_transform(crs = "EPSG:3857")
+    if(!st_crs(original) == "EPSG:3857"){
+        original <- original %>% st_as_sf() %>% st_transform(crs = "EPSG:3857")
+      }
+      if(!st_crs(intersecting) == st_crs(data)){
+        intersecting <- intersecting %>% st_as_sf() %>% st_transform(crs = st_crs(data))
+     }
   }
   return(original[which(lengths(geos_intersects_matrix(original$geometry, 
                                                 intersecting$geometry)) > 0),])
