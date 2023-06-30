202c5d0a28734d1211bc70ed05b608d1daa324b2
Noah Dasanaike <noahdasanaike@college.harvard.edu>
2023-06-30 10:35:28 -0400

    force coordinate projection

--- base functions/osm_data_sf.R
+++ base functions/osm_data_sf.R
@@ -1,18 +1,23 @@
-osm_data_sf <- function(shapes, key, value, return, quiet = TRUE){
+osm_data_sf <- function(shapes, key, value, quiet = TRUE){
   library(osmdata)
   data <- tibble()
   for(i in 1:nrow(shapes)){
     if(quiet == FALSE){cat("\r", (i - 1) / nrow(shapes))}
-    split_shapes <- st_wrap_dateline(shapes[i,]) %>% st_cast("POLYGON") %>% 
+    if(quiet == FALSE){cat("\n", "checking for cross-dateline")}
+    split_shapes <- st_wrap_dateline(st_transform(shapes[i,], crs = "EPSG:3857")) %>% st_cast("POLYGON") %>% 
+      st_transform(crs = 4326) %>%
       rowwise() %>%
       mutate(is_east = ifelse(st_coordinates(st_centroid(geometry))[,1] > 0, 1, 0)) %>% 
       group_by(is_east) %>% 
       summarize(geometry = st_union(geometry))
     
+    if(!st_crs(split_shapes) == 4326){split_shapes <- st_transform(split_shapes, crs = 4326)}
+    
     for(y in 1:nrow(split_shapes)){
       boolFalse <- FALSE
       while(boolFalse == FALSE)
       {
+        if(quiet == FALSE){cat("\n", "trying download")}
         tryCatch({
           dat1 <- opq(bbox = st_bbox(split_shapes$geometry[y]),
                       timeout = 10000) %>%
@@ -22,7 +27,7 @@ osm_data_sf <- function(shapes, key, value, return, quiet = TRUE){
         }, error = function(e){
         }, finally = {})
       }
-      
+      if(quiet == FALSE){cat("\n", "filtering shapes")}
       dat2 <- dat1[["osm_polygons"]] %>%
         st_filter(split_shapes$geometry[y])
       if(nrow(dat2) == 0){
