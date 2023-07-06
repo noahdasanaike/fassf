fbb14daec953229a2e73837a3870d69251a034d3
Noah Dasanaike <noahdasanaike@college.harvard.edu>
2023-07-06 13:52:52 -0400

    fix split polygon validity

--- base functions/osm_data_sf.R
+++ base functions/osm_data_sf.R
@@ -40,11 +40,13 @@ osm_data_sf <- function(shapes, key, value, additional, additional_type,
         if(quiet == FALSE){cat("\n", paste0(round(100 * (i - 1) / nrow(shapes), 2), "%"))}
         if(quiet == FALSE){cat("\n", "checking for cross-dateline polygons")}
         split_shapes <- st_wrap_dateline(st_transform(shapes[i,], crs = "EPSG:3857")) %>% st_cast("POLYGON") %>% 
-          st_transform(crs = 4326) %>%
+          st_transform(crs = 4326)  %>%
+          st_make_valid() %>%
           rowwise() %>%
           mutate(is_east = ifelse(st_coordinates(st_centroid(geometry))[,1] > 0, 1, 0)) %>% 
           group_by(is_east) %>% 
-          summarize(geometry = st_union(geometry))
+          summarize(geometry = st_union(geometry)) %>%
+          st_make_valid()
         
         if(!st_crs(split_shapes) == 4326){split_shapes <- st_transform(split_shapes, crs = 4326)}
         
