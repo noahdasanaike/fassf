8a224df6fb21dc049775161b91350acc971c59da
Noah Dasanaike <noahdasanaike@college.harvard.edu>
2023-07-06 11:26:16 -0400

    add subsetting to avoid memory exhaustion

--- base functions/ruggedness_mad.R
+++ base functions/ruggedness_mad.R
@@ -1,4 +1,6 @@
-ruggedness_mad <- function(polygons, z_level = 7, quiet = FALSE, override_size_check = FALSE){
+ruggedness_mad <- function(polygons, z_level = 7, quiet = FALSE, 
+                           override_size_check = FALSE, 
+                           split = TRUE, split_chunks = 10){
   require(spatialEco)
   require(elevatr)
   
@@ -13,22 +15,50 @@ ruggedness_mad <- function(polygons, z_level = 7, quiet = FALSE, override_size_c
     return(sqrt(sum(((median(x) - x)^2))))
   }
   
-  if(quiet == FALSE){print("converting polygons")}
-  
   polygons <- st_as_sf(polygons)
   
-  mun_sf <- as(polygons, "Spatial")
-  
-  if(quiet == FALSE){print("generating elevation raster")}
-  
-  tryCatch({elev <- get_elev_raster(mun_sf, z = z_level, verbose = !quiet, 
-                                    override_size_check = override_size_check)}, 
-           error = function(e){if(grepl(e, pattern = "gdal_utils warp: an error occured")){
-             return(cat("\r", "gdal_utils warp error: likely too much memory requested"))
-           }}, finally = {})
-  
-  if(quiet == FALSE){print("calculating ruggedness")}
-  
-  return(as.numeric(exactextractr::exact_extract(elev, polygons, fun = tri_fun,
-                                                 progress = !quiet)))
+  if(split == TRUE){
+    if(quiet == FALSE){cat("\r", 
+                           "generating elevation raster and calculating ruggedness, by chunk")}
+    
+    chunk <- nrow(shapefiles) / split_chunks
+    r  <- rep(1:ceiling(nrow(shapefiles) / chunk), each = chunk)[1:nrow(shapefiles)]
+    d <- split(polygons, r)
+
+    ruggedness <- c()
+    
+    for(i in 1:split_chunks){
+      if(quiet == FALSE){cat("\r", paste0(round(100 * i / split_chunks, 2), "%"))}
+      elev <- NA
+      
+      mun_sf <- as(d[[i]], "Spatial")
+      
+      tryCatch({elev <- get_elev_raster(mun_sf, z = z_level, verbose = !quiet, 
+                                        override_size_check = override_size_check)}, 
+               error = function(e){if(grepl(e, pattern = "gdal_utils warp: an error occured")){
+                 return(cat("\r", "gdal_utils warp error: likely too much memory requested"))
+               }}, finally = {})
+      
+      ruggedness <- c(ruggedness, as.numeric(exactextractr::exact_extract(elev, d[[i]], fun = tri_fun,
+                                                                          progress = !quiet)))
+    }
+  }else{
+    if(quiet == FALSE){print("converting polygons to spatial")}
+    
+    mun_sf <- as(polygons, "Spatial")
+    
+    if(quiet == FALSE){print("generating elevation raster")}
+    
+    tryCatch({elev <- get_elev_raster(mun_sf, z = z_level, verbose = !quiet, 
+                                      override_size_check = override_size_check)}, 
+             error = function(e){if(grepl(e, pattern = "gdal_utils warp: an error occured")){
+               return(cat("\r", "gdal_utils warp error: likely too much memory requested"))
+             }}, finally = {})
+    
+    if(quiet == FALSE){print("calculating ruggedness")}
+    
+    ruggedness <- as.numeric(exactextractr::exact_extract(elev, polygons, fun = tri_fun,
+                                                          progress = !quiet))
+  }
+  return(ruggedness)
 }
