8ab9547a202369bfc90cb9321aaaca04320494ab
Noah Dasanaike <noahdasanaike@college.harvard.edu>
2023-07-13 11:43:30 -0400

    add geometry union

--- base functions/faster_st_distance.R
+++ base functions/faster_st_distance.R
@@ -1,5 +1,8 @@
-faster_st_distance <- function(a, b, centroid = TRUE){
+faster_st_distance <- function(a, b, centroid = TRUE, summarize_geometry = TRUE){
   cores <- parallel::detectCores() - 1
+  if(nrow(b) > 0 & summarize_geometry == TRUE){
+    b <- b %>% summarize(geometry = st_union(geometry))
+  }
   if(centroid == TRUE){
     centroids <- st_centroid(a)
     return(unlist(nngeo::st_nn(centroids,
