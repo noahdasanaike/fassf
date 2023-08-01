44544b03445842347d665af3aa512d3f133e37b8
Noah Dasanaike <noahdasanaike@college.harvard.edu>
2023-08-01 19:20:46 -0400

    update files

--- base functions/faster_st_filter.R
+++ base functions/faster_st_filter.R
@@ -7,9 +7,8 @@ faster_st_filter <- function(original, intersecting, quiet = FALSE){
         original <- original %>% st_as_sf() %>% st_transform(crs = "EPSG:3857")
       }
       if(!st_crs(intersecting) == st_crs(data)){
-        intersecting <- intersecting %>% st_as_sf() %>% st_transform(crs = st_crs(data))
+        intersecting <- intersecting %>% st_as_sf() %>% st_transform(crs = st_crs(original))
      }
   }
-  return(original[which(lengths(geos_intersects_matrix(original$geometry, 
-                                                intersecting$geometry)) > 0),])
+  return(original[which(lengths(geos_intersects_matrix(st_geometry(original), st_geometry(intersecting))) > 0),])
 }
--- base functions/raster_polygon_values.R
+++ base functions/raster_polygon_values.R
@@ -10,10 +10,13 @@ raster_polygon_values <- function(raster, polygons, quiet = FALSE){
   
   raster_file <- project(raster_file, crs(polygons))
   
+  if(!quiet == TRUE){"obtaining estimates"}
+  
   get_values <- function(i, polygons, raster_file, quiet){
     if(!quiet == TRUE){cat("\r", i / nrow(polygons))}
     polygon <- st_as_sf(st_make_valid(polygons$geometry[i]))
-    x <- crop(raster_file, ext(polygon))
+    x <- tryCatch(crop(raster_file, ext(polygon)), error = function(e){NA})
+    if(typeof(x) == "logical"){return(NA)}
     y <- mask(x, polygon)
     return(values(y, na.rm = T))
   }
