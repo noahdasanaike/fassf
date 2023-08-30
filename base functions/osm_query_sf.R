8b7ea9189581e7ca86d95f07f0132c5553a18d7d
Noah Dasanaike <noahdasanaike@g.harvard.edu>
2023-08-30 15:22:04 -0400

    add polygons

--- base functions/osm_query_sf.R
+++ base functions/osm_query_sf.R
@@ -3,6 +3,8 @@ osm_query_sf <- function(query, filter, attempts = 10, quiet = FALSE){
   require(jsonlite)
   require(sf)
   
+  httr::set_config(httr::config(http_version = 0))
+  
   base_url <- "https://nominatim.openstreetmap.org/search"
   
   for(i in 1:length(query)){
@@ -10,23 +12,30 @@ osm_query_sf <- function(query, filter, attempts = 10, quiet = FALSE){
     z <- 0
     while(z < attempts){
       Sys.sleep(1)
-      response <- GET(url = base_url, query = list(q = query[i], format = "json"))
+      response <- GET(url = base_url, query = list(q = query[i], format = "geojson",
+                                                   polygon_geojson = 1))
       if(!grepl(content(response, "text"), pattern = "502 Bad Gateway")){
         break
       }else{
         z <- z + 1
       }
     }
-    result <- fromJSON(content(response, "text"), flatten = TRUE)
+    result <- fromJSON(content(response, "text"), flatten = TRUE)$features
     if(length(result) == 0){
       object <- data.frame(query = query[i],
                            missing = TRUE)
     }else{
-      object <- st_as_sf(result,
-                         coords = c("lon", "lat"),
-                         remove = FALSE,
-                         crs = 4326)
-      object <- object[, !colnames(object) %in% c("licence", "boundingbox")] 
+      result$geometry <- NA
+      for(p in 1:nrow(result)){
+        geom <- tibble(lon = result$geometry.coordinates[[p]][,,1],
+                       lat = result$geometry.coordinates[[p]][,,2]) %>%
+          st_as_sf(coords = c("lon", "lat"), crs = 4326) %>% 
+          summarize(geometry = st_combine(geometry)) %>%
+          st_cast("POLYGON")
+        result$geometry[p] <- st_geometry(geom)
+      }
+      object <- st_as_sf(result, crs = 4326)
+      object <- object[, !colnames(object) %in% c("licence", "bbox", "geometry.coordinates")] 
       object$query <- query[i]; object$missing <- FALSE
       if(!missing(filter)){
         object <- st_filter(st_transform(object, crs = "EPSG:3857"),
