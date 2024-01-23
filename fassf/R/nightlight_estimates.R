3a615d4736fbe87ad7803295c4177e7c64bae02c
Noah Dasanaike <noahdasanaike@g.harvard.edu>
2024-01-23 16:39:25 -0500

    Add files via upload

--- fassf/R/nightlight_estimates.R
+++ fassf/R/nightlight_estimates.R
@@ -12,6 +12,7 @@ nightlight_estimates <- function(years, polygons, identifier, fun = "mean",
   if(!identifier %in% colnames(polygons)){
     return("missing identifier from polygon names")
   }
+  # https://figshare.com/articles/dataset/Harmonization_of_DMSP_and_VIIRS_nighttime_light_data_from_1992-2018_at_the_global_scale/9828827
   urls <- data.frame(year = 1992:2021,
                      urls = c("https://figshare.com/ndownloader/files/17626052",
                               "https://figshare.com/ndownloader/files/17626055",
---
+++ fassf/R/raster_polygon_values_faster.R
@@ -0,0 +1,49 @@
+#' @export
+
+raster_polygon_values_faster <- function(raster, polygons, quiet = FALSE, fun = NULL,
+                                         chunks = 20,
+                                         drop_na = TRUE,
+                                         replace_missing = NA){
+  require(sf)
+  require(raster)
+  require(stars)
+  require(terra)
+  require(pbapply)
+
+  raster_file <- rast(raster)
+
+  if(!quiet == TRUE){"re-projecting raster file"}
+
+  raster_file <- tryCatch({terra::project(raster_file, crs(polygons))}, error = function(e){return(e)})
+  if(grepl(raster_file, pattern = "incorrect number of values")){
+    polygons <- st_transform(polygons, crs = "EPSG:3857")
+    raster_file <- rast(raster)
+    raster_file <- tryCatch({project(raster_file, crs(polygons))}, error = function(e){return(e)})
+  }
+  if(!quiet == TRUE){"obtaining estimates"}
+
+  get_values <- function(i, chunks, polygons, raster_file, quiet, fun, replace_missing, drop_na){
+    polygon <- st_as_sf(st_union(st_make_valid(st_geometry(polygons)[chunk_indices[[i]]])))
+    x <- tryCatch(crop(raster_file, ext(polygon)), error = function(e){NA})
+    if(typeof(x) == "logical"){return(NA)}
+    y <- mask(x, polygon)
+    z <- st_as_sf(st_as_stars(y))
+
+    return(intersection_fun(st_geometry(polygons)[chunk_indices[[i]]], z,
+                            colnames(z)[1], fun = fun, quiet = TRUE, replace_missing = replace_missing))
+  }
+
+  chunk_indices <- split(1:nrow(polygons), cut(seq_along(1:nrow(polygons)), chunks, labels = FALSE))
+
+  final_values <- pbsapply(FUN = get_values,
+                           X = 1:length(chunk_indices),
+                           chunks = chunk_indices,
+                           polygons = polygons,
+                           raster_file = raster_file,
+                           quiet = quiet,
+                           replace_missing = replace_missing,
+                           fun = fun,
+                           drop_na = drop_na)
+
+  return(c(final_values))
+}
