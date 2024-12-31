71c54bd6a4082c45bd54c36ce3b1d085f834b39e
Noah Dasanaike <noahdasanaike@college.harvard.edu>
2024-12-31 14:42:08 -0500

    add future requirement

--- fassf/R/better_voronoi_polygons.R
+++ fassf/R/better_voronoi_polygons.R
@@ -1,5 +1,8 @@
 #' @export
 better_voronoi_polygons <- function(x, intersection, workers = parallel::detectCores() - 1, parallel = FALSE, quiet = FALSE) {
+  require(sf)
+  require(future)
+  
   x <- st_transform(x, crs = 4326)
   sf::sf_use_s2(FALSE)
   
