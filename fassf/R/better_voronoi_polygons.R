495522e08287d0a62f65d02c9e353d4b048cf8ec
Noah Dasanaike <noahdasanaike@college.harvard.edu>
2025-01-13 16:34:07 -0500

    implement raster-based intersection with terra

--- fassf/R/better_voronoi_polygons.R
+++ fassf/R/better_voronoi_polygons.R
@@ -1,50 +1,69 @@
 #' @export
-better_voronoi_polygons <- function(x, intersection, quiet = FALSE) {
+better_voronoi_polygons <- function (x, intersection, quiet = FALSE) {
   require(sf)
   require(future)
   require(furrr)
   require(progressr)
+  require(terra)
   
-  x <- st_transform(x, crs = 4326)
-  bbox <- st_bbox(st_transform(intersection, crs = 4326))
+  original_crs <- st_crs(x)
+  bbox <- st_bbox(st_transform(intersection, crs = st_crs(original_crs)))
   sf::sf_use_s2(FALSE)
-  
   if (length(x) != length(unique(x))) {
     return("not all geometries are unique")
   }
-  
   require(deldir)
   require(sp)
-  if (!quiet) print("converting to spatial format")
+  if (!quiet){
+    print("converting to spatial format")
+  }
   x <- as_Spatial(x)
-  
-  if (!quiet) print("constructing Thiessen polygons")
-  crds <- if (.hasSlot(x, "coords")) x@coords else x
+  if (!quiet){
+    print("constructing Thiessen polygons")
+  }
+  crds <- if (.hasSlot(x, "coords")) {
+    x@coords} else {
+      x
+    }
   z <- deldir(crds[, 1], crds[, 2], rw = c(bbox[1], bbox[3], 
                                            bbox[2], bbox[4]))
   w <- tile.list(z)
-  
   polys <- vector(mode = "list", length = length(w))
   for (i in seq(along = polys)) {
     pcrds <- cbind(w[[i]]$x, w[[i]]$y)
     pcrds <- rbind(pcrds, pcrds[1, ])
     polys[[i]] <- Polygons(list(Polygon(pcrds)), ID = as.character(i))
   }
-  
-  if (!quiet) print("converting to sf format")
+  if (!quiet) {
+    print("converting to sf format")
+  }
   SP <- SpatialPolygons(polys)
-  final <- st_make_valid(st_geometry(st_as_sf(SP)))
-  st_crs(final) <- 4326
+  intersection <- st_make_valid(st_transform(intersection, 
+                                             crs = original_crs))
   
-  final <- st_make_valid(st_transform(final, crs = "EPSG:3857"))
-  intersection <- st_make_valid(st_transform(intersection, crs = "EPSG:3857"))
-  final_b <- map(final, ~st_intersection(st_set_crs(st_sfc(.), "EPSG:3857"), 
-                                         st_geometry(intersection)))
-  object <- st_as_sf(tibble(geometry = final)) %>% 
-    rowwise() %>% 
-    mutate(geometry_b = ifelse(length(st_intersection(geometry, intersection)) == 1,
-                               st_intersection(geometry, intersection), NA))
-  object$geometry_b <- st_sfc(object$geometry_b, crs = "EPSG:3857")
-
-  return(object$geometry_b)
+  # Modified conversion section
+  terra1_sf <- st_as_sf(SP)
+  
+  ###
+  test <- terra1_sf
+  test$intersects <- lengths(st_intersects(st_set_crs(test, original_crs), intersection))
+  test %>% 
+    st_set_crs(original_crs) %>%
+    ggplot() +
+    geom_sf(aes(fill = intersects)) +
+    geom_sf(data = intersection, fill = NA, color = "red") +
+    geom_sf(data = st_as_sf(x), color = "green")
+  ###
+  
+  terra1_sf_valid <- st_make_valid(terra1_sf)
+  
+  terra1 <- vect(terra1_sf_valid)
+  
+  if(st_geometry_type(intersection) == "GEOMETRYCOLLECTION"){
+    df_int <- st_intersection(st_set_crs(terra1_sf_valid, original_crs), intersection)
+  }else{
+    terra2 <- vect(intersection)
+    df_int <- intersect(terra1, terra2) %>% st_as_sf()
+  }
+  return(st_set_crs(st_sfc(df_int$geometry), original_crs))
 }
